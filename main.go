71ee38b267adf9b57960de16f0a3c6b4d9985cab
Parsa Yousefi <parsa@arvancloud.com>
2021-03-01 10:06:51 +0330

    LogLevel add \ Change config from file to ENV

--- main.go
+++ main.go
@@ -2,36 +2,49 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
 	"strconv"
 	"time"
 
+	//app "consumer/app"
+
 	log "github.com/sirupsen/logrus"
 
-	"github.com/BurntSushi/toml"
 	"github.com/Shopify/sarama"
 )
 
-type config struct {
-	KafkaWorkers   int       `toml:"kafka_output_workers"`
-	KafkaBrokers   []string  `toml:"kafka_brokers"`
-	KafkaTopic     string    `toml:"kafka_topic"`
-	KafkaBatchSize int       `toml:"kafka_batch_size"`
-	KafkaQueueSize int       `toml:"kafka_queue_size"`
-	KafkaFlushTime int       `toml:"kafka_flush_time_ms"`
-	LogLevel       log.Level `toml:"loglevel"`
+func logLevel() log.Level {
+	if value, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		switch value {
+		case "trace":
+			return log.TraceLevel
+		case "debug":
+			return log.DebugLevel
+		case "info":
+			return log.InfoLevel
+		case "warn":
+			return log.WarnLevel
+		case "error":
+			return log.ErrorLevel
+		case "fatal":
+			return log.FatalLevel
+		case "panic":
+			return log.WarnLevel
+		default:
+			return log.InfoLevel
+		}
+	}
+	return log.InfoLevel
 }
 
 func main() {
-	var conf config
-	_, err := toml.DecodeFile("./conf.toml", &conf)
-
-	if err != nil {
-		panic(err)
+	log.SetLevel(logLevel())
+	topic := "epoch"
+	if value, ok := os.LookupEnv("TOPIC"); ok {
+		topic = value
 	}
-
 	c := make(chan os.Signal, 1)
 
 	signal.Notify(c, os.Interrupt)
@@ -40,55 +53,187 @@ func main() {
 
 	go func() {
 		oscall := <-c
-		log.Printf("system call:%+v", oscall)
+		log.Debug("system call:%+v", oscall)
 		cancel()
 	}()
 
-	producer, err := newProducer(&conf)
+	producer, err := newProducer()
 	if err != nil {
-		fmt.Println("Could not create producer: ", err)
+		log.Error("Could not create producer: ", err)
 	}
 
-	if err := sendMessage(ctx, producer, conf.KafkaTopic); err != nil {
-		log.Printf("failed to produce:+%v\n", err)
+	if err := sendMessage(ctx, producer, topic); err != nil {
+		log.Error("failed to produce:+%v\n", err)
 	}
 }
 
 func sendMessage(ctx context.Context, producer sarama.SyncProducer, topic string) (err error) {
-	//var wg sync.WaitGroup
+	var counter int64
+
 	go func() {
 		for {
 			time.Sleep(time.Millisecond)
-			msg := prepareMessage(topic, strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10))
-			_, _, err := producer.SendMessage(msg)
+			rawString := strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10)
+			msg := prepareMessage(topic, rawString)
+			partition, offset, err := producer.SendMessage(msg)
 			if err != nil {
-				fmt.Println("%s error occured.", err.Error())
+				log.Panic("error occured.", err)
 			}
+			log.Trace("Counter: ", counter, " Message: ", rawString, " topic: ", topic, " partition: ", partition, " offset: ", offset)
+			counter++
 		}
 	}()
 	<-ctx.Done()
-
-	log.Printf("server stopped")
+	log.Info("messages produced: ", counter)
+	log.Info("server stopped")
 
 	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer func() {
 		cancel()
 	}()
 
-	log.Printf("server exited properly")
+	log.Info("server exited properly")
 
 	return
 }
+func requiredAcks() sarama.RequiredAcks {
+	if value, ok := os.LookupEnv("PRODUCER_REQUIRED_ACKS"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_REQUIRED_ACKS: ", err)
+		}
+		switch valuei {
+		case 0:
+			return sarama.NoResponse
+		case 1:
+			return sarama.WaitForLocal
+		case -1:
+			return sarama.WaitForAll
+		default:
+			return sarama.WaitForLocal
+		}
+	}
+	return sarama.WaitForLocal
+}
 
-func newProducer(conf *config) (sarama.SyncProducer, error) {
+func saramaPartitioner() sarama.PartitionerConstructor {
+	if value, ok := os.LookupEnv("PRODUCER_PARTITIONER"); ok {
+		switch value {
+		case "random":
+			log.Debug("Partitioner: Random")
+			return sarama.NewRandomPartitioner
+		case "hash":
+			log.Debug("Partitioner: Hash")
+			return sarama.NewHashPartitioner
+		case "rr":
+			log.Debug("Partitioner: RoundRobin")
+			return sarama.NewRoundRobinPartitioner
+		default:
+			log.Debug("Partitioner: Hash")
+			return sarama.NewHashPartitioner
+		}
+	}
+	return sarama.NewHashPartitioner
+}
+func newProducer() (sarama.SyncProducer, error) {
 	config := sarama.NewConfig()
-	config.Producer.Partitioner = sarama.NewRandomPartitioner
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	config.Producer.Return.Successes = true
-	config.Producer.Flush.Frequency = time.Duration(conf.KafkaFlushTime) * time.Millisecond
-	config.Producer.Flush.Messages = conf.KafkaBatchSize
-	producer, err := sarama.NewSyncProducer(conf.KafkaBrokers, config)
+	var brokers []string
+	brokers = []string{"localhost:9092"}
 
+	config.Producer.RequiredAcks = requiredAcks()
+	if value, ok := os.LookupEnv("PRODUCER_MAX_MESSAGE_BYTES"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_MAX_MESSAGE_BYTES: ", err)
+		}
+		config.Producer.MaxMessageBytes = valuei
+	}
+	if value, ok := os.LookupEnv("PRODUCER_FLUSH_FREQUENCY"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_FLUSH_FREQUENCY: ", err)
+		}
+		config.Producer.Flush.Frequency = time.Duration(rand.Int31n(int32(valuei))) * time.Millisecond
+	}
+	if value, ok := os.LookupEnv("PRODUCER_FLUSH_MESSAGE"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_FLUSH_MESSAGE: ", err)
+		}
+		config.Producer.Flush.Messages = valuei
+	}
+	if value, ok := os.LookupEnv("PRODUCER_FLUSH_MAX_MESSAGE"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_FLUSH_MAX_MESSAGE: ", err)
+		}
+		config.Producer.Flush.MaxMessages = valuei
+	}
+	config.Producer.Return.Successes = true
+	if value, ok := os.LookupEnv("PRODUCER_RETURN_SUCCESS"); ok {
+		if value == "false" {
+			config.Producer.Return.Successes = false
+		}
+	}
+	if value, ok := os.LookupEnv("PRODUCER_TIMEOUT"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_TIMEOUT: ", err)
+		}
+		config.Producer.Timeout = time.Duration(rand.Int31n(int32(valuei))) * time.Second
+	}
+	config.Producer.Partitioner = saramaPartitioner()
+
+	if value, ok := os.LookupEnv("PRODUCER_RETRY_MAX"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_RETRY_MAX: ", err)
+		}
+		config.Producer.Retry.Max = int(valuei)
+	}
+	if value, ok := os.LookupEnv("PRODUCER_RETRY_BACKOFF"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_RETRY_BACKOFF: ", err)
+		}
+		config.Producer.Retry.Backoff = time.Duration(rand.Int31n(int32(valuei))) * time.Millisecond
+	}
+	if value, ok := os.LookupEnv("PRODUCER_RETURN_ERROR"); ok {
+		if value == "true" {
+			config.Producer.Return.Errors = true
+		} else {
+			config.Producer.Return.Errors = false
+		}
+	}
+	if value, ok := os.LookupEnv("PRODUCER_COMPRESSIONLEVEL"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_COMPRESSIONLEVEL: ", err)
+		}
+		config.Producer.CompressionLevel = valuei
+	}
+	if value, ok := os.LookupEnv("PRODUCER_CLIENTID"); ok {
+		config.ClientID = value
+	}
+	if value, ok := os.LookupEnv("PRODUCER_CHANNELBUFFERSIZE"); ok {
+		valuei, err := strconv.Atoi(value)
+		if err != nil {
+			log.Error("Bad! PRODUCER_CHANNELBUFFERSIZE: ", err)
+		}
+		config.ChannelBufferSize = valuei
+	}
+	if value, ok := os.LookupEnv("PRODUCER_VERSION"); ok {
+		version, err := sarama.ParseKafkaVersion(value)
+		if err != nil {
+			log.Error("version incompatible: ", err)
+		}
+		config.Version = version
+	}
+	if value, ok := os.LookupEnv("PRODUCER_BROKERS"); ok {
+		brokers = []string{value}
+	}
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	log.Info(config.Producer.Partitioner)
 	return producer, err
 }
 
